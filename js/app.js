22c20418118b2a8c31cf4b3a0e02e751674ea1ce
Aden <aden.lofthouse@gmail.com>
2019-10-21 08:58:31 +1100

    final

--- js/app.js
+++ js/app.js
@@ -2,18 +2,27 @@
  * Create a list that holds all of your cards
  */
 
-const cards = ['fa-diamond', 'fa-diamond',
-               'fa-paper-plane-o', 'fa-paper-plane-o',
-               'fa-anchor', 'fa-anchor',
-               'fa-bolt', 'fa-bolt',
-               'fa-cube', 'fa-cube',
-               'fa-leaf','fa-leaf',
-               'fa-bicycle', 'fa-bicycle',
-               'fa-bomb', 'fa-bomb'
-              ];
+const cards = [
+	"fa-diamond",
+	"fa-diamond",
+	"fa-paper-plane-o",
+	"fa-paper-plane-o",
+	"fa-anchor",
+	"fa-anchor",
+	"fa-bolt",
+	"fa-bolt",
+	"fa-cube",
+	"fa-cube",
+	"fa-leaf",
+	"fa-leaf",
+	"fa-bicycle",
+	"fa-bicycle",
+	"fa-bomb",
+	"fa-bomb"
+];
 
 function generateCard(card) {
-      return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
+	return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
 }
 
 /*
@@ -25,19 +34,20 @@ function generateCard(card) {
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
+	let currentIndex = array.length,
+		temporaryValue,
+		randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-    return array;
+	return array;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -50,102 +60,104 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-const moveCounter = document.querySelector('.moves');
+const moveCounter = document.querySelector(".moves");
 let startTime;
 
 let openCards = [];
-const resetButton = document.querySelector('.restart');
-let allCards = document.querySelectorAll('.card');
-
+const resetButton = document.querySelector(".restart");
+let allCards = document.querySelectorAll(".card");
 
 //setup game
-function initGame(){
-      const deck = document.querySelector('.deck');
-      const cardHTML = shuffle(cards).map(function(card){
-          return generateCard(card);
-      });
-      deck.innerHTML = cardHTML.join('');
-      moveCounter.innerHTML = 0;
-      startTime = new Date()
+function initGame() {
+	const deck = document.querySelector(".deck");
+	const cardHTML = shuffle(cards).map(function(card) {
+		return generateCard(card);
+	});
+	deck.innerHTML = cardHTML.join("");
+	moveCounter.innerHTML = 0;
+	startTime = new Date();
 
-      allCards = document.querySelectorAll('.card');
-      allCards.forEach((card) => {
-          card.addEventListener('click', () => {
-            if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
-              openCards.push(card);
-              card.classList.add('open', 'show');
+	allCards = document.querySelectorAll(".card");
+	allCards.forEach(card => {
+		card.addEventListener("click", () => {
+			if (
+				!card.classList.contains("open") &&
+				!card.classList.contains("show") &&
+				!card.classList.contains("match")
+			) {
+				openCards.push(card);
+				card.classList.add("open", "show");
 
-              if (openCards.length === 2){
-                //increment the counter
-                incrementCounter()
-                //check match
-                if (openCards[0].dataset.card === openCards[1].dataset.card){
-                    openCards[0].classList.add('match','open', 'show');
+				if (openCards.length === 2) {
+					//increment the counter
+					incrementCounter();
+					//check match
+					if (openCards[0].dataset.card === openCards[1].dataset.card) {
+						openCards[0].classList.add("match", "open", "show");
 
-                    openCards[1].classList.add('match','open', 'show');
+						openCards[1].classList.add("match", "open", "show");
 
-                    openCards = [];
-                      setTimeout(() => {
+						openCards = [];
+						setTimeout(() => {
+							if (checkWin()) {
+								const finishTime = new Date();
 
-                        if (checkWin()){
-                          const finishTime = new Date()
+								// compare the two
+								const diffTime = finishTime - startTime;
 
-                          // compare the two
-                          const diffTime = finishTime - startTime
+								console.log(Math.round(diffTime / 1000));
 
-                          console.log(Math.round(diffTime/1000))
-
-                          let resetGame = window.confirm(`You win! That took you ${Math.round(diffTime/1000)} seconds. Do you want to play again?`);
-                          if (resetGame) {
-                            initGame();
-                          }
-                        }
-                      }, 500);
-
-                } else {
-                //clear cards if no match
-                setTimeout(() => {
-                  openCards.forEach((card) => {
-                    card.classList.remove('open', 'show');
-                });
-                openCards = [];
-
-              }, 500);
-             }
-            }
-          }
-        });
-      });
+								let resetGame = window.confirm(
+									`You win! That took you ${Math.round(
+										diffTime / 1000
+									)} seconds. Do you want to play again?`
+								);
+								if (resetGame) {
+									initGame();
+								}
+							}
+						}, 500);
+					} else {
+						//clear cards if no match
+						setTimeout(() => {
+							openCards.forEach(card => {
+								card.classList.remove("open", "show");
+							});
+							openCards = [];
+						}, 500);
+					}
+				}
+			}
+		});
+	});
 }
 
 const incrementCounter = () => {
-    const countInt = parseInt(moveCounter.innerHTML)
-    moveCounter.innerHTML = countInt+1;
-    if (countInt >= 10){
-      document.getElementById("star-1").classList.add("hidden");
-    }
-    if (countInt >= 20) {
-        document.getElementById("star-2").classList.add("hidden");
-      }
-    if (countInt >= 30) {
-        document.getElementById("star-3").classList.add("hidden");
-    }
-  }
-
-
+	const countInt = parseInt(moveCounter.innerHTML);
+	moveCounter.innerHTML = countInt + 1;
+	if (countInt >= 10) {
+		document.getElementById("star-1").classList.add("hidden");
+	}
+	if (countInt >= 20) {
+		document.getElementById("star-2").classList.add("hidden");
+	}
+	if (countInt >= 30) {
+		document.getElementById("star-3").classList.add("hidden");
+	}
+};
 
 initGame();
 
-resetButton.addEventListener('click', () => {
-  initGame();
+resetButton.addEventListener("click", () => {
+	initGame();
 });
 
 const checkWin = () => {
-  let isWin = true
-  allCards.forEach((card) => {
-    if(!card.classList.contains('match')) {
-      isWin = false
-    }
-  })
-  return isWin;
-}
+	let isWin = true;
+	allCards.forEach(card => {
+		if (!card.classList.contains("match")) {
+			isWin = false;
+		}
+	});
+	return isWin;
+};
